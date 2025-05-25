b41588572b41d21888cec613a8bb1fc71be79da9
Vipin Negi <vnegi1011@gmail.com>
2025-05-25 07:24:05 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,29 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export const metadata: Metadata = {
-  title: 'Ramante Homes',
-  description: 'Luxury homestays in Dehradun',
-};
+  title: {
+    default: 'Ramante Homes | Luxury Homestays in Dehradun',
+    template: '%s | Ramante Homes',
+  },
+  description: 'Experience luxury homestays in Dehradun at Ramante Homes. Book cozy cottages with stunning views, modern amenities, and a serene environment.',
+  keywords: ['luxury homestays', 'Dehradun homestays', 'Ramante Homes', 'cottage rentals', 'Mussoorie vacations'],
+  openGraph: {
+    title: 'Ramante Homes | Luxury Home-stays in Dehradun',
+    description: 'Discover luxury and serenity at Ramante Homes in Dehradun. Book your stay today!',
+    url: 'https://homestay-zeta.vercel.app/',
+    siteName: 'Ramante-Homes',
+    images: [
+      {
+        url: '/logo.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Ramante Homes Luxury Homestays',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
