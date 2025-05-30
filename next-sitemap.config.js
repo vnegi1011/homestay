47cf152a6c5e9baff2641ac70599b0e3bd2967f1
vnegi1011 <115055146+vnegi1011@users.noreply.github.com>
2025-05-30 23:10:51 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteUrl: 'https://homestay-zeta.vercel.app', // update after deploy
     generateRobotsTxt: true,
-    sitemapSize: 7000,
+    sitemapSize: 10000,
 //   additionalPaths: async (config) => [
 //     await config.transform(config, '/explore'),
 //   ],
